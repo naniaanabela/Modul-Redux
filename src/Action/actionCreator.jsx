ceb44b00ee0357a4be2e429ee4f1fa0b1f2b2c37
Nurcahya Nania Anabela <nania.anabela22@gmail.com>
2020-03-19 08:53:37 +0700

    membuat file actionCreator dan memasukkan code didalamnya

---
+++ src/Action/actionCreator.jsx
@@ -0,0 +1,30 @@
+import 
+{
+    ADD_TODO, 
+    REMOVE_TODO, 
+    TOOGLE_TODO,
+    SET_VISIBILITY_FILTER
+} from './actionsTypes'
+
+let TodoId = 2
+
+export const addTodo = text => ({
+    type: ADD_TODO,
+    id: TodoId++,
+    text
+})
+
+export const deleteTodo = (id) => ({
+    type: REMOVE_TODO,
+    id: id
+})
+
+export const toogleTodo = (id) => ({
+    type: TOGGLE_TODO,
+    id: id
+})
+
+export const setVisibilityFilter = filter => ({
+    type: SET_VISIBILITY_FILTER,
+    filter
+})
